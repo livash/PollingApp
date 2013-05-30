4b1bf27ef6b87d65da498d4300b595ddb1ba5a2e
Olena and Ricky <onrz@app.io>
2013-05-30 10:45:51 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polls::Application.config.secret_token = 'fc5a5f221c9f37a6b18a778f517802b8b5058d67b33abcbc8b3c3ea4a19db0e88790abde7159047292259d0cc27e9c8c19247010da188e5231f93756d6724d15'
